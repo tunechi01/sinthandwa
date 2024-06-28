8eb61195793fdf6789c3d44a8e830ce1bb948df3
tunechi01 <147820457+tunechi01@users.noreply.github.com>
2024-06-28 12:22:38 +0200

    Add files via upload

---
+++ js/index.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int count =0;
+   for(int i=1;i<=6;i++){
+        int a;
+    cout<<"Enter number: "<<endl;
+    cin>>a;
+   count+=a;
+   cout<<"The sum is: "<<count<<endl;
+
+   }
+	return 0;
+}
